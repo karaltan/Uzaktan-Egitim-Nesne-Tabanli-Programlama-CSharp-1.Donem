7f3b7e1a26c6ace9a9e000017c6b2ec0b2e0d3d8
Altan KARAALP <karaltan@gmail.com>
2021-03-12 23:56:37 +0300

    2.hafta
    button, label, textbox, değer alma, değer yazdırma, değişkenler, temrinler

---
+++ 2-13.10.2020/ilkprojem/ilkprojem/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+namespace ilkprojem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {   //PROBLEM: 2 textboxa girilen sayıyı, TOPLA butonuna basınca toplayan 
+            //ve sonucu 3.textboxta gösteren program
+         
+            //GİRİŞ : değişkenler tanımlanır
+            int s1, s2,sonuc;
+
+            //GELİŞME : değerler alınır, işlem yapılır
+            // ** işlemler sağdan sola !!!!!!!!!!!!
+            s1 =Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            sonuc = s1 + s2;
+
+           // sonuc =Convert.ToInt32(textBox3.Text); //HATAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+            //SONUÇ : çıktı
+            textBox3.Text = sonuc.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   // kutuları silmek
+
+            textBox1.Text = "";  // değeri sil
+            textBox2.Clear();    // değeri sil
+            textBox3.Clear();    // değeri sil
+
+            textBox1.Focus(); // imleci ilk kutuya odaklan
+        }
+    }
+}
---
+++ 2-13.10.2020/ortalama/ortalama/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ortalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  //TEMRİN : ORTALAMA HESABI
+            double p1, p2, p3, ort;
+
+            p1 = Convert.ToDouble(textBox1.Text);
+            p2 = Convert.ToDouble(textBox2.Text);
+            p3 = Convert.ToDouble(textBox3.Text);
+
+            ort = (p1 + p2 + p3) / 3;
+
+            textBox4.Text = ort.ToString();
+        }
+    }
+}
